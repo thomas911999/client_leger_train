cb396ab4fad946f72af84e629efec7b6e8debbcc
thomas <thomas911999@hotmail.fr>
2024-03-08 17:37:16 +0100

    add form

---
+++ train/index.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <title>Titre du document</title>
+    <link href="ressources/css/form_user.css" rel="stylesheet">
+  </head>
+<body>
+    <div class="login-box">
+    <h2>Login</h2>
+    <form>
+        <div class="user-box">
+        <input type="text" name="" required="">
+        <label>Username</label>
+    </div>
+    <div class="user-box">
+        <input type="password" name="" required="">
+        <label>Password</label>
+    </div>
+    <a href="#">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Submit
+    </a>
+    </form>
+</div>
+</body>
