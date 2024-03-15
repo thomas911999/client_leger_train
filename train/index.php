417e4d66d89e4fa2bdfb360cc13da0ac6b96f99b
thomas <thomas911999@hotmail.fr>
2024-03-15 14:44:39 +0100

    changement du css , prochaine étape , formulaire d'inscription

--- train/index.php
+++ train/index.php
@@ -1,26 +1,27 @@
 <html>
   <head>
-    <title>Titre du document</title>
+    <title>TrainExpress</title>
     <link href="ressources/css/form_user.css" rel="stylesheet">
   </head>
 <body>
     <div class="login-box">
-    <h2>Login</h2>
-    <form>
+    <h2>Connexion</h2>
+    <form action="verif.php" method="post">
         <div class="user-box">
         <input type="text" name="" required="">
-        <label>Username</label>
+        <label>Utilisateur</label>
     </div>
     <div class="user-box">
         <input type="password" name="" required="">
-        <label>Password</label>
+        <label>mot de passe</label>
     </div>
-    <a href="#">
+    <a href="php/register.php" id="register">Pas encore de compte ? Cliquez ici</a>
+    <a href="#" id="toto">
         <span></span>
         <span></span>
         <span></span>
         <span></span>
-        Submit
+        <input type="submit" value="Se connecter">
     </a>
     </form>
 </div>
