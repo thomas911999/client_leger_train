32b79153d02a86a198472a25983105f23f706361
Maharo <mrandriamahefazo@gmail.com>
2024-05-30 19:42:38 +0200

    Ajout de bdd

--- train/php/bdd.php
+++ train/php/bdd.php
@@ -4,13 +4,13 @@ function DatabaseConnection()
     {
         // Replace these values with your actual database credentials
 $host = getenv('DB_HOST');
-$user = "root";
-$pass = getenv('DB_PASS');
+$user = "maharo";
+$pass = getenv('maharo');
 $name = getenv('DB_NAME');
 
         // Attempt to create a new PDO instance (representing a database connection)
         try {
-            $pdo = new PDO("mysql:host=localhost;dbname=train", $user, "");
+            $pdo = new PDO("mysql:host=localhost;dbname=train", $user, "maharo");
             return $pdo;
         } catch (PDOException $e) {
             // If an exception is caught, fail the test
